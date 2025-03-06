afce3a048c388c73d421ecdfd4796151401609d3
ruirui <yruirui96@gmail.com>
2025-03-06 10:33:11 +0800

    路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  "base": "/bigScreen/",
   plugins: [vue()],
+  "base": "/bigScreen/",
    optimizeDeps: {
     include: ['vue', 'vue-router', 'vuex']  // 确保必要的依赖被正确解析
   },
