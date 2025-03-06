b04c57c47eacbfc6c277f0b60c53989fdc456751
ruirui <yruirui96@gmail.com>
2025-03-06 19:51:13 +0800

    00

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/bigScren-page/'
-    : '/',
+    ? '/bigScreen/'
+    : './',
   plugins: [vue()],
    optimizeDeps: {
     include: ['vue', 'vue-router', 'vuex']  // 确保必要的依赖被正确解析
